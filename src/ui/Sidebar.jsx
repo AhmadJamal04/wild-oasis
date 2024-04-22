67c7a01a9905089a085d25bdfde5a582d15dd4e6
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-21 17:10:31 -0700

    sidebar component adjusted

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,16 +1,22 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
+const StyledSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-right: 1px solid var(--color-grey-100);
 
-const StyledSidebar=styled.aside`
-    background-color: var(--color-grey-0);
-    padding: 1.2rem 4.8rem;
-    border-right: 1px solid var(--color-grey-100);
-   
-    grid-row: 1 / 3;
-   
-`
+  grid-row: 1 / 3;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
 
 export default function Sidebar() {
   return (
-    <StyledSidebar>Sidebar</StyledSidebar>
-  )
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
