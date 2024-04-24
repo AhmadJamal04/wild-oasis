73b3a31cbfee135337c2b9ebc9b3411a4e9787e6
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-24 12:17:54 -0700

    fetched cabins data for testing

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    getCabins()
+      .then((data) => {
+        console.log(data);
+        const imageUrl = data[0].image;
+        console.log(imageUrl)
+        setUrl(imageUrl);
+      })
+      .catch((error) => {
+        console.error("Error fetching cabin data:", error);
+      });
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src={url} alt="cabin img" />
     </Row>
   );
 }
