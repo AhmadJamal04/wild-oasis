37ab41c5f77eca8d967dc6dfb6f40d2e5c041576
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-25 11:21:36 -0700

    old useeeffect hook removed

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,27 +2,19 @@ import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
-  const [url, setUrl] = useState("");
-  useEffect(() => {
-    getCabins()
-      .then((data) => {
-        console.log(data);
-        const imageUrl = data[0].image;
-        console.log(imageUrl)
-        setUrl(imageUrl);
-      })
-      .catch((error) => {
-        console.error("Error fetching cabin data:", error);
-      });
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img src={url} alt="cabin img" />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>filter/sort</p>
+      </Row>
+      <Row>
+        <CabinTable/>
+      </Row>
+    </>
   );
 }
 
