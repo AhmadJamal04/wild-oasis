d702aee2c77e7ecd1d06130700c64ac9b4f8a719
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-25 11:21:09 -0700

    cabin table view configure

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import Row from "../../ui/Row";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +28,31 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+export default function CabinTable() {
+  const {
+    isLoading,
+    data: cabins,
+    error,
+  } = useQuery({
+    queryKey: ["cabin"],
+    queryFn: getCabins,
+  });
+  console.log(cabins);
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return (
+    <Table role="table">
+      <TableHeader role="header">
+        <div></div>
+        <div>cabin</div>
+        <div>capacity</div>
+        <div>price</div>
+        <div>discount</div>
+        <div></div>
+      </TableHeader>
+     {cabins.map(cabin=><CabinRow cabin={cabin} key={cabin.id}/>)}
+    </Table>
+  );
+}
