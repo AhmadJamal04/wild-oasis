865509869ee08399224c4bb4c298a12b7360a17b
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-29 15:57:49 -0700

    creating cabin function configure for uploading image to supabase bucket

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -16,13 +16,31 @@ export async function deleteCabin(Id) {
   }
 }
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${
+    newCabin.name.image
+  }`.replaceAll("/","");
+  const imagePath = `https://ngcnmikizbbofyizolyp.supabase.co/storage/v1/object/public/cabin-images/${imageName}`;
+  //https://ngcnmikizbbofyizolyp.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-04-25T19%3A40%3A59.096Z
+  // 1 create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
+    console.log("cabindata",data)
   if (error) {
     console.log(error);
     throw new error("Cabin could not be created");
+  }
+  // 2 upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3 delete cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError)
+    throw new Error("cabin image could not be uploaded and the cabin was not created")
   }
   return data;
 }
