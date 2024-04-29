ca3b5a8c28ee6a416c5fc96456c8aa5805a7cb77
AhmadJamal04 <engr.jamal04@gmail.com>
2024-04-29 00:59:51 -0700

    create cabinform component used in cabin  page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const[showForm,setShowForm]=useState(false)
   return (
     <>
       <Row type="horizontal">
@@ -14,6 +17,8 @@ function Cabins() {
       <Row>
         <CabinTable/>
       </Row>
+      <Button variation="primary" onClick={()=>setShowForm(show=>!show)}>add new cabin</Button>
+      {showForm && <CreateCabinForm setShowForm={setShowForm} showForm={showForm}/>}
     </>
   );
 }
