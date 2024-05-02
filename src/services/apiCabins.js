b158ef3e197feb5f33b84ae8dd64b5c3139d13a4
AhmadJamal04 <engr.jamal04@gmail.com>
2024-05-01 17:01:47 -0700

    createEditCabin function configured

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase, { supabaseUrl } from "./supaBase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
@@ -16,18 +16,25 @@ export async function deleteCabin(Id) {
   }
 }
 export async function createEditCabin(newCabin, id) {
-  const hasImagePath= newCabin.image?.startsWith?.(supabaseUrl);
-  const imageName = `${Math.random()}-${newCabin.name.image}`.replaceAll("/","");
-  const imagePath = hasImagePath? newCabin.image:`https://ngcnmikizbbofyizolyp.supabase.co/storage/v1/object/public/cabin-images/${imageName}`;
+  const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+  console.log("has image path", hasImagePath);
+  const imageName = `${Math.random()}-${newCabin.name.image}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = hasImagePath
+    ? newCabin.image
+    : `https://ngcnmikizbbofyizolyp.supabase.co/storage/v1/object/public/cabin-images/${imageName}`;
 
   // 1 create/edit cabin
-  let query =  supabase.from("cabins");
+  let query = supabase.from("cabins");
 
   // A Create
-  if (!id) query= query.insert([{ ...newCabin, image: imagePath }]);
+  if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
 
   //B edit
-  if (id)query= query.update({ ...newCabin, image: imagePath }).eq("id", id);
+  if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
 
   const { data, error } = await query.select().single();
   console.log("cabindata", data);
@@ -36,7 +43,7 @@ export async function createEditCabin(newCabin, id) {
     throw new error("Cabin could not be created");
   }
   // 2 upload image
-  if (hasImagePath) return data;
+  if (hasImagePath) return data; //if image already uploaded then dont upload it again
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
