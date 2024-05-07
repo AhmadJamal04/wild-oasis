14d23504c26bbf7ade836372f586bee6c410e660
AhmadJamal04 <engr.jamal04@gmail.com>
2024-05-06 23:31:19 -0700

    some jsx is moved to a new component which is then used in cabin page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,26 +1,20 @@
-
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import { useState } from "react";
-import Button from "../ui/Button";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
+
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const[showForm,setShowForm]=useState(false)
   return (
-    
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
         <p>filter/sort</p>
       </Row>
       <Row>
-        <CabinTable setShowForm={setShowForm} showForm={showForm}/>
+        <CabinTable />
       </Row>
-      <Button variation="primary" onClick={()=>setShowForm(show=>!show)}>add new cabin</Button>
-      
-      {showForm && <CreateCabinForm />} 
+      <AddCabin />
     </>
   );
 }
