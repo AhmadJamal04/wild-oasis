112568a543cc553a9a353b23ef7906595012776b
AhmadJamal04 <engr.jamal04@gmail.com>
2024-05-13 12:29:16 -0700

    tbale opening as model removed

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -6,27 +6,19 @@ import CabinTable from "./CabinTable";
 // compund component pattern in which the model component will manage its state whether it is open or close, unlike the addCABIN component which decides whether the model will open or close
 export default function AddCabin() {
   return (
-    <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button
-          variation="primary"
-          size="medium"
-          > add new cabin</Button>
-          </Modal.Open>
+    <div>
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button variation="primary" size="medium">
+           
+            add new cabin
+          </Button>
+        </Modal.Open>
         <Modal.Window name="cabin-form">
           <CreateCabinForm />
         </Modal.Window>
-
-        <Modal.Open opens="table">
-        <Button
-          variation="primary"
-          size="medium"
-          > show table</Button>
-          </Modal.Open >
-        <Modal.Window name="table">
-          <CabinTable />
-        </Modal.Window>
-    </Modal>
+      </Modal>
+    </div>
   );
 }
 
