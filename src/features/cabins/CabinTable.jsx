d965d6a833340fada92b37b67ef6688ded031244
Ahmad Jamal <ENGR.JAMAL04@GMAIL.COM>
2024-06-06 19:09:27 -0700

    reusable table component is used instead of already made table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,15 +5,9 @@ import Spinner from "../../ui/Spinner";
 
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
+import Table from "../../ui/Table";
 
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
 
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -38,15 +32,15 @@ export default function CabinTable() {
   }
   console.log("cabins", cabins);
   return (
-    <Table role="table">
-      <TableHeader role="header">
+    <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+      <Table.Header >
         <div></div>
         <div>cabin</div>
         <div>capacity</div>
         <div>price</div>
         <div>discount</div>
         <div></div>
-      </TableHeader>
+      </Table.Header>
       {cabins.map((cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
       ))}
