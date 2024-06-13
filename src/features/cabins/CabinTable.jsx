f733f0b7d81324b68aa62bad1dc5e2443a6373db
Ahmad Jamal <ENGR.JAMAL04@GMAIL.COM>
2024-06-13 11:40:46 +0500

    applied the render props pattern

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,28 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
+
 import Spinner from "../../ui/Spinner";
 
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
-
-
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
 
 export default function CabinTable() {
   const { isLoading, cabins } = useCabins();
@@ -33,7 +15,7 @@ export default function CabinTable() {
   console.log("cabins", cabins);
   return (
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header >
+      <Table.Header>
         <div></div>
         <div>cabin</div>
         <div>capacity</div>
@@ -41,9 +23,11 @@ export default function CabinTable() {
         <div>discount</div>
         <div></div>
       </Table.Header>
-      {cabins.map((cabin) => (
-        <CabinRow cabin={cabin} key={cabin.id} />
-      ))}
+      <Table.Body
+        data={cabins}
+        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+      />
+     
     </Table>
   );
 }
