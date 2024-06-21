321eb8539a34b849e42b6c2e26e31b58da4c6bf7
Ahmad Jamal <ENGR.JAMAL04@GMAIL.COM>
2024-06-21 16:22:37 +0500

    sort functionality added

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -15,6 +15,8 @@ export default function CabinTable() {
   if (isLoading) {
     return <Spinner />;
   }
+
+  // filter 
  const filterValue=searchParams.get("discount")||"all";
  let filteredCabins;
  if(filterValue==="all")filteredCabins=cabins
@@ -22,6 +24,12 @@ export default function CabinTable() {
   filteredCabins=cabins.filter((cabin)=>cabin.discount===0)
  if(filterValue==="with-discount") 
   filteredCabins=cabins.filter((cabin)=>cabin.discount>0)
+
+//  sort
+const sortBy= searchParams.get("sortBy")||"startDate-asc";
+const[field,direction]=sortBy.split("-");
+const modifier= direction==="asc"? 1:-1
+const sortedCabins= filteredCabins.sort((a,b)=>(a[field]-b[field])*modifier)
   return (
     <Menus>
     <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -34,7 +42,8 @@ export default function CabinTable() {
         <div></div>
       </Table.Header>
       <Table.Body
-        data={filteredCabins}
+        // data={filteredCabins}
+        data={sortedCabins}
         render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
       />
      
