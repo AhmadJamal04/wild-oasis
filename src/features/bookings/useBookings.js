4590e8204a24c2f6eb2a545c23eac3f0f5c2d1c6
Ahmad Jamal <ENGR.JAMAL04@GMAIL.COM>
2024-07-09 12:57:08 +0500

    for a server side filtering we configureg filter object which is then used in api bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
-export function useBookings(){
-    const { isLoading, data: bookings,error } = useQuery({
-        queryFn: getBookings,
-        queryKey: ["bookings"],
-      });
-      return {isLoading,bookings,error};
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryFn: ()=>getBookings({filter}),
+    queryKey: ["bookings",filter],
+  });
+  return { isLoading, bookings, error };
 }
